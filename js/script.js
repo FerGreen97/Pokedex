34034716b52067e195da3477d4f8085dc3e9de9c
Pablo Chaparro <pablo.47.114@gmail.com>
2024-08-04 21:36:41 -0300

    actualizacion subTipo pokemon

--- js/script.js
+++ js/script.js
@@ -5,18 +5,15 @@ fetch("./database.json")
     .then(data =>{
         for(const pokemon of data){
             contenidoTarjetasHTML.innerHTML += `
-            <div class="tarjetaPokemon">
+            <div class="tarjetaPokemon"">
                     <div class="contenedorImg">
                         <img src="${pokemon.imgPokemon}" >
                     </div>
                     <span class="identificador">#${pokemon.idPokemon}</span>
                     <span class="nombre">${pokemon.nombre}</span>
-                    <div class="iconos">
+                    <div class="iconos" id="${pokemon.idPokemon}">
                         <div class="contenedorIcon">
                             <img src="${pokemon.tipoPokemon}">                            
-                        </div>
-                        <div class="contenedorIcon">
-                            <img src="${pokemon.subTipo}">                            
                         </div>
                     </div>
                     <button class="informacion">Informacion</button>
@@ -24,5 +21,19 @@ fetch("./database.json")
             `
         }
     }
-
-    )
+)
+fetch("./database.json")
+    .then(response => response.json())
+    .then(data =>{
+        for(const pokemon of data){
+            const iconoPokemonHTML = document.getElementById(`${pokemon.idPokemon}`)
+            if(pokemon.subTipo != "no"){
+                iconoPokemonHTML.innerHTML += `
+                    <div class="contenedorIcon">
+                            <img src="${pokemon.subTipo}">                            
+                    </div>
+                `
+            }
+        }
+    }
+)
