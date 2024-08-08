61d9bee1fe57e1fe85698410749033d6450a8496
Pablo Chaparro <pablo.47.114@gmail.com>
2024-08-08 09:27:32 -0300

    carga de tarjetas desde la api terminada

--- js/script.js
+++ js/script.js
@@ -2,18 +2,22 @@ const contenidoTarjetasHTML = document.getElementById("tarjetas")
 
 /* Tarjetas cargadas desde la API pokeapi */
 let offset = 0
+
 const pokeapi="https://pokeapi.co/api/v2/pokemon?offset="+offset+"limit=20"
+
 const cargarTarjetas = (apiURL) => {
     fetch(apiURL)
         .then(response => response.json())
         .then(data =>{
             for(const pokemon of data.results){
                 contenidoTarjetasHTML.innerHTML +=`
-                <div class="tarjetaPokemon" ">
+                <div class="tarjetaPokemon" id="${pokemon.name}">
                 <span class="nombre">${pokemon.name}</span>
-                <button class="informacion" onclick="mensaje()">Informacion</button>
                 </div>
                 `
+            }
+            for(const pokemon of data.results){
+                rellenarTarjeta("https://pokeapi.co/api/v2/pokemon/"+pokemon.name,pokemon.name)
             }
         }
     )
@@ -25,10 +29,41 @@ const cargarMas = () => {
 }
 cargarTarjetas(pokeapi)
 
+const rellenarTarjeta = (pokemonURL,pokename) =>{
+    const contenedorPokemonHTML = document.getElementById(pokename)
+    fetch(pokemonURL)
+        .then(response => response.json())
+        .then(data =>{
+            contenedorPokemonHTML.innerHTML = `
+            <div class="contenedorImg">
+                <img src="${data.sprites.other.dream_world.front_default}" alt="imagen de ${pokename}">
+            </div>
+            <span class="identificador">#0${data.id}</span>
+            `+ contenedorPokemonHTML.innerHTML + `
+            <div class="iconos" id="icono${pokename}">
+                
+            </div>
+            `
+            for(const tipoPokemon of data.types){
+                const conenedorIconosHTML = document.getElementById("icono"+pokename)
+                conenedorIconosHTML.innerHTML +=`
+                <div class="contenedorIcon">
+                    <img src="./img/icon/${tipoPokemon.type.name}.svg" alt="icono de pokemon tipo ${tipoPokemon.type.name}">
+                </div>
+                `
+            }
+            contenedorPokemonHTML.innerHTML += `
+                <button class="informacion" onclick="mensaje()">Informacion</button>
+            </div>
+            `
+        })
+}
+
+
 /*tarjetas cargadas con archivo JSON local*/
 
 /* fetch("./database.json")
-    .then(response => response.json())
+.then(response => response.json())
     .then(data =>{
         for(const pokemon of data){
             if(pokemon.idPokemon<25){
@@ -72,7 +107,7 @@ fetch("./database.json")
         }
     }
 )
-*/
+ */
 const mensaje = () => {
     console.log("holamundo")
 } 
